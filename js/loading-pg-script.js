9b6ae83c98c48f505092aa047fe07b577e7b4e50
beaalmeidas <beatrizalmeidabss@gmail.com>
2024-09-04 11:01:26 -0300

    feat: adicionando html, css e js da página de carregamento

---
+++ js/loading-pg-script.js
@@ -0,0 +1,8 @@
+
+// Função para redirecionar da página de carregamento para a página principal
+function redirectToIndex() {
+    window.location.href = "../index-page/index-structure.html";
+}
+
+// Definindo tempo para execução da função de redirecionamento (4 segundos)
+setTimeout(redirectToIndex, 4000);
