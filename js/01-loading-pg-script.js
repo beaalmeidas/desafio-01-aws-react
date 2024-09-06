ba63f4fdc3b6a2de5e6fc5c84409c2a035b089bd
Auto Logon <autologon@CSED.LAB>
2024-09-06 08:40:06 -0300

    feat: adicionando seta de navegação nas páginas

--- js/01-loading-pg-script.js
+++ js/01-loading-pg-script.js
@@ -1,7 +1,6 @@
-
 // Função para redirecionar da página de carregamento para a página principal
 function redirectToIndex() {
-    window.location.href = "../html/screen-01-structure.html";
+    window.location.href = "../html/02-presentation1-structure.html";
 }
 
 // Definindo tempo para execução da função de redirecionamento (4 segundos)
